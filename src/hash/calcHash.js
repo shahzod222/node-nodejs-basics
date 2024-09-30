d37512a1a9e7c8d76c49591f2a5d53fafebf6bee
shahzod222 <shahzodubaydullaev2206@gmail.com>
2024-09-30 10:37:48 +0500

    feat: complete hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { createHash } from "crypto";
+import path from "path";
+import fs from "fs";
+const __dirname = import.meta.dirname;
+
 const calculateHash = async () => {
-    // Write your code here 
+  const reader = fs.createReadStream(
+    path.join(__dirname, "files", "fileToCalculateHashFor.txt")
+  );
+
+  reader.on("data", (chunk) => {
+    const hashed = createHash("sha256").update(chunk).digest("hex");
+    console.log(hashed);
+  });
 };
 
 await calculateHash();
