20da41dcc15a3a030d9d721dfded05ca6979230b
shahzod222 <shahzodubaydullaev2206@gmail.com>
2024-10-02 17:28:33 +0500

    feat: complete zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+import fs from "fs";
+import zlib from "zlib";
+import path from "path";
+const __dirname = import.meta.dirname;
+
 const compress = async () => {
-    // Write your code here 
+  const zip = zlib.createGzip();
+  const read = fs.createReadStream(
+    path.join(__dirname, "files", "fileToCompress.txt")
+  );
+  const write = fs.createWriteStream(
+    path.join(__dirname, "files", "archive.gz")
+  );
+
+  read.pipe(zip).pipe(write);
+
+  write.on("finish", () => {
+    console.log("Compressed successfully");
+  });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import fs from "fs";
+import zlib from "zlib";
+import path from "path";
+const __dirname = import.meta.dirname;
+
 const decompress = async () => {
-    // Write your code here 
+  const unzip = zlib.createUnzip();
+
+  const read = fs.createReadStream(path.join(__dirname, "files", "archive.gz"));
+  const write = fs.createWriteStream(
+    path.join(__dirname, "files", "fileToCompress.txt")
+  );
+
+  read.pipe(unzip).pipe(write);
+
+  write.on("finish", () => {
+    console.log("Decompressed successfully");
+  });
 };
 
 await decompress();
