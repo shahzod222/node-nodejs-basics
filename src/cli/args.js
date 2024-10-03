a47b7ac382561706005e654565b2a361b96156da
shahzod222 <shahzodubaydullaev2206@gmail.com>
2024-10-03 18:59:52 +0500

    feat: complete cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+
+  const res = args
+    .filter((_, i) => i % 2 === 0)
+    .map((value, i) => `${value.slice(2)} is ${args[i * 2 + 1]}`)
+    .join(", ");
+
+  console.log(res);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+  const res = Object.keys(process.env)
+    .filter((value) => value.startsWith("RSS_"))
+    .map((value) => `${value}=${process.env[value]}`)
+    .join(", ");
+
+  console.log(res);
 };
 
 parseEnv();
