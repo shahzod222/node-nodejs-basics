f786de42ceec33d02922bd59e63a3269dfe50a49
shahzod222 <shahzodubaydullaev2206@gmail.com>
2024-10-07 20:07:52 +0500

    feat: complete all tasks

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,25 @@
+import path from "path";
+import childProcess from "node:child_process";
+const __dirname = import.meta.dirname;
+const scriptPath = path.join(__dirname, "files", "script.js");
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = childProcess.spawn("node", [scriptPath, ...args]);
+
+  process.stdin.pipe(child.stdin);
+
+  child.stdout.on("data", (data) => {
+    process.stdout.write(data.toString());
+  });
+
+  child.on("error", (err) => {
+    console.log("Error: ", err);
+  });
+
+  child.on("exit", (code) => {
+    console.log(`Child process exited with code ${code}`);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["one", "two", "three", "four"]);
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,42 @@
+import path from "path";
+import os from "os";
+import { Worker } from "node:worker_threads";
+const __dirname = import.meta.dirname;
+
 const performCalculations = async () => {
-    // Write your code here
+  const target = path.join(__dirname, "worker.js");
+  const cpus = os.cpus().length;
+  const workerPromises = [];
+  const workerThreads = [];
+
+  for (let i = 0; i < cpus; i++) {
+    const promise = new Promise((res, rej) => {
+      const worker = new Worker(target);
+      workerThreads.push(worker);
+
+      worker.postMessage(10 + i);
+
+      worker.on("message", (result) =>
+        res({ status: "resolved", data: result })
+      );
+
+      worker.on("error", () => rej({ status: "error", data: null }));
+
+      worker.on("exit", (code) => {
+        if (code !== 0) rej({ status: "error", data: null });
+      });
+    });
+
+    workerPromises.push(promise);
+  }
+
+  const resWorkers = await Promise.allSettled(workerPromises);
+
+  const result = resWorkers.map((res) => res.value);
+
+  console.log(result);
+
+  workerThreads.forEach((worker) => worker.terminate());
 };
 
 await performCalculations();
